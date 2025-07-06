21d53804514d0349392ea8e86ff6b088503d9f66
Sanjeevni Dey <sanjeevnidey.info@gmail.com>
2025-07-07 00:39:56 +0530

    display messages in admin page

--- admin.php
+++ admin.php
@@ -1,3 +1,116 @@
+<?php
+  session_start();
+  include 'partials/_dbconnect.php';
+  include 'partials/_navbar.php';
+
+  if($_SERVER['REQUEST_METHOD'] == "GET" && isset($_GET['deleteId'])){
+    $book_id = $_GET['deleteId'];
+    try{
+      $query = "DELETE FROM `booklist` WHERE `book_id` = $book_id";
+      $result = mysqli_query($conn, $query);
+
+      if($result){
+        $_SESSION['adminSuccess'] = "Record Deleted";
+        header("location: admin.php");
+        exit();
+      }
+      else{
+        $_SESSION['adminFail'] = "Record is not deleted.";
+        header("location: admin.php");
+        exit();
+      }
+      exit();
+    }
+    catch(mysqli_sql_exception $e){
+      $_SESSION['adminFail'] = "Database error: " . $e->getMessage();
+    }
+  }
+
+  if (isset($_SESSION['adminSuccess'])) {
+    echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+          <strong>Success!</strong> ' . htmlspecialchars($_SESSION['adminSuccess']) . '
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>';
+    unset($_SESSION['adminSuccess']);
+  }
+  else if (isset($_SESSION['adminFail'])) {
+      echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Failed!</strong> ' . htmlspecialchars($_SESSION['adminFail']) . '
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>';
+      unset($_SESSION['adminFail']);
+  }
+
+  if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit_edit'])){
+    $book_id = $_POST['bookId'];
+    $title = $_POST['titleEdit'];
+    $desc = $_POST['descEdit'];
+    $image = $_POST['imageEdit'];
+    $retail_price = $_POST['retail_priceEdit'];
+    $org_price = $_POST['org_priceEdit'];
+    $genre = $_POST['genreEdit'];
+    $pages = $_POST['pagesEdit'];
+    $publisher = $_POST['publisherEdit'];
+    $isbn = $_POST['isbnEdit'];
+    $category = $_POST['categoryEdit'];
+    
+    try{
+      $query = "UPDATE `booklist` SET `image` = '$image', `book_desc` = '$desc', `po_category_id` = '$category', `book_title` = '$title', `ratings` = '★★★★★ (4.8/5)', `book_disc_price` = '$retail_price', `original_price` = '$org_price', `genre` = '$genre', `page` = '$pages', `publisher` = '$publisher', `ISBN` = '$isbn' WHERE `book_id` = $book_id";
+      $result= mysqli_query($conn, $query);
+
+      if($result){
+        $_SESSION['adminSuccess'] = "Record Updated successfully";
+        header("Location: /my_book_store/admin.php");
+        exit();
+      }
+      else{
+        $_SESSION['adminFail'] = "No Record Updated. Try Again!";
+        header("Location: /my_book_store/admin.php");
+        exit();
+      }
+    }
+    catch(mysqli_sql_exception $e){
+      $_SESSION['adminFail'] = "Database error: " . $e->getMessage();
+    }
+  }
+
+  if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['add_book'])){
+    $title = $_POST['title'];
+    $desc = $_POST['desc'];
+    $image = $_POST['image'];
+    $retail_price = $_POST['retail_price'];
+    $org_price = $_POST['org_price'];
+    $genre = $_POST['genre'];
+    $pages = $_POST['pages'];
+    $publisher = $_POST['publisher'];
+    $isbn = $_POST['isbn'];
+    $category = $_POST['category'];
+    // $ratings = $_POST['ratings'];
+
+    try{
+      $query = "INSERT INTO `booklist` 
+      (`book_id`, `image`, `book_desc`, `po_category_id`, `book_title`, `ratings`, `book_disc_price`, `original_price`, `timestamp`, `genre`, `page`, `publisher`, `ISBN`) 
+      VALUES 
+      (NULL, '$image' , '$desc', '$category', '$title', '★★★★☆ (4.6/5)', '$retail_price', '$org_price', current_timestamp(), '$genre', '$pages', '$publisher', '$isbn')";
+
+      $result = mysqli_query($conn, $query);
+      if($result){
+          $_SESSION['adminSuccess'] = "Record added successfully";
+          header("Location: /my_book_store/admin.php");
+          exit();
+      }
+      else{
+        $_SESSION['adminFail'] = "No Record Added. Try Again!";
+        header("Location: /my_book_store/admin.php");
+        exit();
+      }
+    }
+    catch(mysqli_sql_exception $e){
+      $_SESSION['adminFail'] = "Database error: " . $e->getMessage();
+    }
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,20 +125,6 @@
   <link rel="stylesheet" href="css/_admin.css" />
 </head>
 <body>
-    <?php
-    include 'partials/_dbconnect.php'; 
-    include 'partials/_navbar.php';
-    
-    if($_SERVER['REQUEST_METHOD'] == "GET" && isset($_GET['deleteId'])){
-      $book_id = $_GET['deleteId'];
-      $query = "DELETE FROM `booklist` WHERE `booklist`.`book_id` = $book_id";
-      $result = mysqli_query($conn, $query);
-
-      header("location: admin.php");
-    }
-  ?>
-
-  <!-- Modal -->
   <div class="modal fade custom-modal" id="myModal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
@@ -41,23 +140,23 @@
               <div class="row g-3">
                 <div class="col-md-4">
                 <label for="imageEdit" class="form-label">Image</label>
-                <textarea id="imageEdit" name="imageEdit" class="form-control" placeholder="Upload image" rows="4"></textarea>
+                <textarea id="imageEdit" name="imageEdit" class="form-control" placeholder="Upload image" rows="4" required></textarea>
               </div>
               <div class="col-md-4">
                 <label for="titleEdit" class="form-label">Book Title</label>
-                <textarea id="titleEdit" name="titleEdit" class="form-control" placeholder="Enter title" rows="4"></textarea>
+                <textarea id="titleEdit" name="titleEdit" class="form-control" placeholder="Enter title" rows="4" required></textarea>
               </div>
               <div class="col-md-4">
                 <label for="publisherEdit" class="form-label">Publisher</label>
-                <input type="text" id="publisherEdit" name="publisherEdit" class="form-control" placeholder="Enter publisher" />
+                <input type="text" id="publisherEdit" name="publisherEdit" class="form-control" placeholder="Enter publisher" required />
               </div>
               <div class="col-md-4">
                 <label for="retail_priceEdit" class="form-label">Retail_price</label>
-                <input type="number" id="retail_priceEdit" name="retail_priceEdit" class="form-control" placeholder="Enter retail_price" />
+                <input type="number" id="retail_priceEdit" name="retail_priceEdit" class="form-control" placeholder="Enter retail_price" required />
               </div>
               <div class="col-md-4">
                 <label for="org_priceEdit" class="form-label">Original Price</label>
-                <input type="number" id="org_priceEdit" name="org_priceEdit" class="form-control" placeholder="Enter original price" />
+                <input type="number" id="org_priceEdit" name="org_priceEdit" class="form-control" placeholder="Enter original price" required/>
               </div>
               <!-- <div class="col-md-4">
                 <label for="ratings" class="form-label">Ratings</label>
@@ -65,21 +164,25 @@
               </div> -->
               <div class="col-md-4">
                 <label for="genreEdit" class="form-label">Genre</label>
-                <input type="text" id="genreEdit" name="genreEdit" class="form-control" placeholder="Enter genre" />
+                <input type="text" id="genreEdit" name="genreEdit" class="form-control" placeholder="Enter genre" required/>
               </div>
               <div class="col-md-4">
                 <label for="pagesEdit" class="form-label">Number of Pages</label>
-                <input type="number" id="pagesEdit" name="pagesEdit" class="form-control" placeholder="Enter pages" />
+                <input type="number" id="pagesEdit" name="pagesEdit" class="form-control" placeholder="Enter pages" required/>
               </div>
               <div class="col-md-4">
                 <label for="isbnEdit" class="form-label">ISBN</label>
-                <input type="number" id="isbnEdit" name="isbnEdit" class="form-control" placeholder="Enter ISBN number" />
+                <input type="number" id="isbnEdit" name="isbnEdit" class="form-control" placeholder="Enter ISBN number" required/>
+              </div>
+              <div class="col-md-4">
+                <label for="categoryEdit" class="form-label">Category</label>
+                <input type="text" id="categoryEdit" name="categoryEdit" class="form-control" placeholder="Enter Category" required/>
               </div>
             </div>
-        
+            </div>
             <div class="col mt-4 mb-4">
               <label for="descEdit" class="form-label">Book Description</label>
-              <textarea id="descEdit" name="descEdit" class="form-control" placeholder="Enter description" rows="7"></textarea>
+              <textarea id="descEdit" name="descEdit" class="form-control" placeholder="Enter description" rows="7" required></textarea>
             </div>
 
             <div class="modal-footer">
@@ -91,25 +194,6 @@
       </div>
     </div>
   </div>
-
-    <?php
-      if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit_edit'])){
-        $book_id = $_POST['bookId'];
-        $title = $_POST['titleEdit'];
-        $desc = $_POST['descEdit'];
-        $image = $_POST['imageEdit'];
-        $retail_price = $_POST['retail_priceEdit'];
-        $org_price = $_POST['org_priceEdit'];
-        $genre = $_POST['genreEdit'];
-        $pages = $_POST['pagesEdit'];
-        $publisher = $_POST['publisherEdit'];
-        $isbn = $_POST['isbnEdit'];
-
-        $query = "UPDATE `booklist` SET `image` = '$image', `book_desc` = '$desc', `book_title` = '$title', `ratings` = '★★★★★ (4.8/5)', `book_disc_price` = '$retail_price', `original_price` = '$org_price', `genre` = '$genre', `page` = '$pages', `publisher` = '$publisher', `ISBN` = '$isbn' WHERE `book_id` = $book_id";
-        $result= mysqli_query($conn, $query);
-      }
-    
-    ?>
 
   <div class="container">
 
@@ -153,19 +237,19 @@
         <div class="row g-3">
           <div class="col-md-4">
             <label for="image" class="form-label">Image</label>
-            <input type="text" id="image" name="image" class="form-control" placeholder="Upload image" />
+            <input type="text" id="image" name="image" class="form-control" placeholder="Upload image" required/>
           </div>
           <div class="col-md-4">
             <label for="title" class="form-label">Book Title</label>
-            <input type="text" id="title" name="title" class="form-control" placeholder="Enter title" />
+            <input type="text" id="title" name="title" class="form-control" placeholder="Enter title" required/>
           </div>
           <div class="col-md-4">
             <label for="retail_price" class="form-label">Retail_price</label>
-            <input type="number" id="retail_price" name="retail_price" class="form-control" placeholder="Enter retail_price" />
+            <input type="number" id="retail_price" name="retail_price" class="form-control" placeholder="Enter retail_price" required/>
           </div>
           <div class="col-md-4">
             <label for="org_price" class="form-label">Original Price</label>
-            <input type="number" id="org_price" name="org_price" class="form-control" placeholder="Enter original price" />
+            <input type="number" id="org_price" name="org_price" class="form-control" placeholder="Enter original price" required/>
           </div>
           <!-- <div class="col-md-4">
             <label for="ratings" class="form-label">Ratings</label>
@@ -173,54 +257,36 @@
           </div> -->
           <div class="col-md-4">
             <label for="genre" class="form-label">Genre</label>
-            <input type="text" id="genre" name="genre" class="form-control" placeholder="Enter genre" />
+            <input type="text" id="genre" name="genre" class="form-control" placeholder="Enter genre" required/>
           </div>
           <div class="col-md-4">
             <label for="pages" class="form-label">Number of Pages</label>
-            <input type="number" id="pages" name="pages" class="form-control" placeholder="Enter pages" />
+            <input type="number" id="pages" name="pages" class="form-control" placeholder="Enter pages" required/>
           </div>
           <div class="col-md-4">
             <label for="publisher" class="form-label">Publisher</label>
-            <input type="text" id="publisher" name="publisher" class="form-control" placeholder="Enter publisher" />
+            <input type="text" id="publisher" name="publisher" class="form-control" placeholder="Enter publisher" required/>
           </div>
           <div class="col-md-4">
             <label for="isbn" class="form-label">ISBN</label>
-            <input type="number" id="isbn" name="isbn" class="form-control" placeholder="Enter ISBN number" />
+            <input type="number" id="isbn" name="isbn" class="form-control" placeholder="Enter ISBN number" required/>
+          </div>
+          <div class="col-md-4">
+            <label for="category" class="form-label">Category</label>
+            <input type="text" id="category" name="category" class="form-control" placeholder="Enter category" required/>
           </div>
         </div>
         <div class="col mt-4 mb-4">
-              <label for="desc" class="form-label">Book Description</label>
-              <textarea id="desc" name="desc" class="form-control" placeholder="Enter description" rows="4"></textarea>
-            </div>
-          <div class="col-12 mt-3">
-            <button type="submit" name="add_book" id="add_book" class="btn btn-success">
-              <i class="bi bi-plus-circle"></i> Add Book
-            </button>
-          </div>
+            <label for="desc" class="form-label">Book Description</label>
+            <textarea id="desc" name="desc" class="form-control" placeholder="Enter description" rows="4" required></textarea>
+        </div>
+        <div class="col-12 mt-3">
+          <button type="submit" name="add_book" id="add_book" class="btn btn-success">
+            <i class="bi bi-plus-circle"></i> Add Book
+          </button>
+        </div>
       </form>
     </div>
-
-    <?php
-        if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['add_book'])){
-            $title = $_POST['title'];
-            $desc = $_POST['desc'];
-            $image = $_POST['image'];
-            $retail_price = $_POST['retail_price'];
-            $org_price = $_POST['org_price'];
-            $genre = $_POST['genre'];
-            $pages = $_POST['pages'];
-            $publisher = $_POST['publisher'];
-            $isbn = $_POST['isbn'];
-            // $ratings = $_POST['ratings'];
-
-            $query = "INSERT INTO `booklist` 
-            (`book_id`, `image`, `book_desc`, `po_category_id`, `book_title`, `ratings`, `book_disc_price`, `original_price`, `timestamp`, `genre`, `page`, `publisher`, `ISBN`) 
-            VALUES 
-            (NULL, '$image', '$desc', '1', '$title', '★★★★☆ (4.6/5)', '$retail_price', '$org_price', current_timestamp(), '$genre', '$pages', '$publisher', '$isbn')";
-
-            $result = mysqli_query($conn, $query);
-        }
-    ?>
 
     <!-- Book Table with Search -->
     <div class="form-section mt-4">
@@ -265,7 +331,7 @@
                 <td>".$rows['ISBN']."</td>
 
                 <td>
-                  <button type='button' class='edit btn btn-sm btn-warning mb-2' id=".$rows['book_id']." data-bs-toggle='modal' data-bs-target='#myModal'><i class='bi bi-pencil-square'></i> Edit</button>
+                  <button style='width:75px' type='button' class='edit btn btn-sm btn-warning mb-2' id=".$rows['book_id']." data-bs-toggle='modal' data-bs-target='#myModal'><i class='bi bi-pencil-square'></i>Edit</button>
                   <button type='button' class='delete btn btn-sm btn-danger' id=".$rows['book_id']."><i class='bi bi-trash'></i> Delete</button>
                 </td>
                 <td style= 'display: none;'>".$rows['book_desc']."</td>
